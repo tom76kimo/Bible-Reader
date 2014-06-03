5da2289ac779bd8e584087c965afa3586f19dce3
tom76kimo <tom76kimo@gmail.com>
2014-06-03 16:34:00 +0800

    add function of showing up updates log

--- public/js/app/view/main.js
+++ public/js/app/view/main.js
@@ -12,6 +12,9 @@ define([
 		initialize: function(){},
 		render: function(){
 			this.$el.html(this.template({}));
+			this.$('#updatesLog').on('show.bs.modal', function (e) {
+				$(this).find('#logBody').html('<iframe src="https://docs.google.com/document/d/12oNIfYpWdFBL03toVqxpRJmqICosa1WSnCne5IGUZxs/pub?embedded=true" width="550" height="500"></iframe>');
+			});
 		}
 	});
 });
